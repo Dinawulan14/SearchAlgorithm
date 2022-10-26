871148da1a0be8c84a3c9292db72a45e43f7912d
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-26 16:16:29 +0700

    Menambahkan perulangan while

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,5 +8,26 @@ namespace SearchAlgorithm
 {
     class Program
     {
-        //Array to be 
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine
+                        ("\nArray should have minimum 1 and maximum 20 lements. \n");
+            }
+            //Accept array 
+        }
     }
