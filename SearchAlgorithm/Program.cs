96ad185efedff691e9ada7bcbcecc85e38e12eab
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-26 16:25:11 +0700

    menambahkan string

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -28,6 +28,16 @@ namespace SearchAlgorithm
                     Console.WriteLine
                         ("\nArray should have minimum 1 and maximum 20 lements. \n");
             }
-            //Accept array 
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("--------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + "+");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);   
+            }
         }
     }
