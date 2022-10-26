47e1b18d04f11564341fe7776937dbaafdc9bad0
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-26 17:24:32 +0700

    menambahkan case 1, 2, 3, default

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -105,7 +105,53 @@ namespace SearchAlgorithm
                 Console.WriteLine("\n Number of comparison: " + ctr);
                 Console.Write("\nContinue search (y/n); ");
                 ch = char.Parse((Console.ReadLine().ToUpper()));
-            }while((ch == 'y'));
+            } while ((ch == 'y'));
+        }
+        static void Main(string[] args)
+        {
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("1. Linear Search");
+                    Console.WriteLine("2. Binary Search");
+                    Console.WriteLine("3. Exit");
+                    Console.Write("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("---------------");
+                            Console.WriteLine("Linear Search");
+                            Console.WriteLine("---------------");
+                            myList.input();
+                            myList.LinearSearch();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("---------------");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine("---------------");
+                            myList.input();
+                            myList.BinarySearch();
+                            break;
+                        case 3:
+                            Console.WriteLine(" Exit ");
+                            break;
+                        default:
+                            Console.WriteLine(" Error ");
+                            break;
+
+                    }
+                }
+            }
+        }
 
                 
 
