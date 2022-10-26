a72e904d2e820e8fda47e2bab73c8254a3bf6423
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-10-26 16:50:57 +0700

    menambahkan method LinearSearch

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -67,6 +67,25 @@ namespace SearchAlgorithm
                         upperbound = mid - 1;
                     mid = (lowerbound + upperbound) / 2;
                     ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at positian" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison :" + ctr);
+
+                Console.Write("\nContinue search (y/n)!");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
+
+            public void LinearSearch()
+            {
+                char ch;
+
+
+            }
+                
 
 
                 }
